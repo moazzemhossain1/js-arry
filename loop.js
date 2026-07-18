5c143136da5f3b97bc53e305200dd2fce14b0e3e
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-19 07:29:39 +0800

    for of loop

---
+++ loop.js
@@ -0,0 +1,22 @@
+const numbers=[23,23,43,23,45,32,432,21,32,343];
+for(number of numbers){
+    // console.log(number);
+    if(number%4===0){
+        continue;
+    }
+    console.log("Creent number is:", number)
+}
+
+
+
+const players=['abule','kabule','nabule','cabule'];
+for(player of players){
+    console.log(player)
+}
+
+
+
+const furits=['apple','banana','goba','connate'];
+for(furit of furits){
+    console.log(furit)
+}
