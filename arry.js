fea816ea03e81bd79cbe6ec040d4910a36d2844e
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-19 06:23:49 +0800

    useing arry

--- arry.js
+++ arry.js
@@ -2,4 +2,23 @@ const teme=['Rahime','Karime','jober','halime','dilem','kalime'];
 console.log(teme[3]);
 teme[0]='Missie';
 console.log(teme);
-console.log(teme.length)
+console.log(teme.length);
+
+
+
+
+
+const players=['abule','nabule','kablue','chablue','dauble'];
+console.log(players[2]);
+console.log(players);
+players[1]='chagule';
+console.log(players);
+
+
+
+const furits=['apple','banana','gouaba','marry'];
+console.log(furits);
+furits[2]='coconate';
+console.log(furits);
+console.log(furits.length);
+console.log(furits[1]);
