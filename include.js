46f89103cc9110e47f4cf11b608c2af4ff935e79
moazzemhossain1 <moazzemhossain202025@gmail.com>
2026-07-19 07:17:24 +0800

    using include ,concat ,joned,indexOf

---
+++ include.js
@@ -0,0 +1,19 @@
+const numbers=[32,33,4,54,52,56,75,43,];
+console.log(numbers);
+
+console.log(numbers.includes(56));
+
+const num=numbers.includes(11);
+console.log(num);
+
+console.log(numbers.indexOf(54));
+const nub=[3,2,1];
+const x=numbers.concat(nub);
+console.log(x);
+
+
+console.log(numbers.join("  "))
+
+
+
+
